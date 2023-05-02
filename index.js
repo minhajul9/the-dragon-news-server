a7f4014424f4bf102ae1e5b94b1f628e2137d3f5
minhajul9 <110691447+minhajul9@users.noreply.github.com>
2023-05-02 19:32:04 +0600

    category wise data load

--- index.js
+++ index.js
@@ -8,24 +8,36 @@ const news = require('./data/news.json');
 
 app.use(cors())
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('Dragon is coming')
 })
 
-app.get('/categories', (req, res)=>{
+app.get('/categories', (req, res) => {
     res.send(categories)
 })
 
-app.get('/news', (req, res) =>{
+app.get('/news', (req, res) => {
     res.send(news)
 })
 
-app.get('/news/:id', (req, res) =>{
+app.get('/news/:id', (req, res) => {
     const id = req.params.id;
-    const selectedNews = news.find( n=> n._id === id);
+    const selectedNews = news.find(n => n._id === id);
     res.send(selectedNews)
 })
 
-app.listen(port, ()=>{
+app.get('/categories/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    if (id === 0) {
+        res.send(news)
+    }
+    else {
+
+        const categoryNews = news.filter(n => parseInt(n.category_id) === id);
+        res.send(categoryNews)
+    }
+})
+
+app.listen(port, () => {
     console.log(`Dragon api is running on port: ${port}`);
 })
